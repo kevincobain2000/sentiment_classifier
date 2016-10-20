c1a4c7b4e4fdf3861621e08152f4dd9620fcee05
MrLokans <trikster1911@gmail.com>
2016-10-20 07:28:51 +0300

    Docs scripts translated to python 3

--- docs/temp/conf.py
+++ docs/temp/conf.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 # -*- coding: utf-8 -*-
 #
 # sentiment_classifier documentation build configuration file, created by
@@ -11,7 +12,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -40,8 +42,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'sentiment_classifier'
-copyright = u'2012, Pulkit Kathuria'
+project = 'sentiment_classifier'
+copyright = '2012, Pulkit Kathuria'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -183,8 +185,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'sentiment_classifier.tex', u'sentiment\\_classifier Documentation',
-   u'Pulkit Kathuria', 'manual'),
+  ('index', 'sentiment_classifier.tex', 'sentiment\\_classifier Documentation',
+   'Pulkit Kathuria', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,8 +215,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'sentiment_classifier', u'sentiment_classifier Documentation',
-     [u'Pulkit Kathuria'], 1)
+    ('index', 'sentiment_classifier', 'sentiment_classifier Documentation',
+     ['Pulkit Kathuria'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -227,8 +229,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'sentiment_classifier', u'sentiment_classifier Documentation',
-   u'Pulkit Kathuria', 'sentiment_classifier', 'One line description of project.',
+  ('index', 'sentiment_classifier', 'sentiment_classifier Documentation',
+   'Pulkit Kathuria', 'sentiment_classifier', 'One line description of project.',
    'Miscellaneous'),
 ]
 
